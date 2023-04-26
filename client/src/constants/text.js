4fda7af315fc350a2e628e17a74f558fc82b9132
Musire <Mcmusire@gmail.com>
2023-04-26 16:58:34 -0600

    Updated text in the constants file

--- client/src/constants/text.js
+++ client/src/constants/text.js
@@ -10,5 +10,64 @@ export const ComponentText = {
         title: 'Bring everyone together to build better products.',
         content: 'Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view.',
         button: 'Get Started'
+    }, 
+    about: {
+        title: "What's different about Manage?",
+        content: "Manage provides all the functionality your team needs, without the complexity. Our software is tailor-made for modern digital product teams.",
+        card: {
+            0 : {
+                title: "Track company-wide progress",
+                content: "See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way done to the smallest of details. Never lose sight of the bigger picture again."
+            },
+            1 : {
+                title: "Advanced built-in reports",
+                content: "Set internal delivery estimates and track progress toward company goals. Our customisable dashboard helps you build out the reports you need to keep key stakeholders informed."
+            },
+            2 : {
+                title: "Everything you need in one place",
+                content: "Stop jumping from one service to another to communicate, store files, track tasks and share documents. Manage offers an all-in-one team productivity solution."
+            }
+        }
+    },
+    testimonials : {
+        title: "What they've said",
+        button: "Get Started",
+        slides: {
+            0 : {
+                name: "Anisha Li",
+                content: "“Manage has supercharged our team's workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.”"
+            },
+            1 : {
+                name: "Ali Bravo",
+                content: "“We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.”"
+            },
+            2 : {
+                name: "Richard Watts",
+                content: "“Manage allows us to provide structure and process. It keeps us organized and focused. I can't stop recommending them to everyone I talk to!”"
+            },
+            3 : {
+                name: "Shanai Gough",
+                content: "“Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.”"
+            },
+        }
+    },
+    contact: {
+        title: "Simplify how your team works today.",
+        button: "Get Started"
+    },
+    footer: {
+        search: "Updates in your inbox…",
+        button: "Go",
+        copywrite: "Copyright 2020. All Rights Reserved",
+        links: {
+            0 : "Home",
+            1 : "Pricing",
+            2 : "Products",
+            3 : "About Us",
+            4 : "Careers",
+            5 : "Community",
+            6 : "Privacy Policy"
+        }
+        
     }
 }
